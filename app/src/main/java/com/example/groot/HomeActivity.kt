4a08f2999263ede03ade69710f3f84f57a78b587
Jay Gheewala <jaygheewala157@gmail.com>
2024-08-02 09:48:52 +0530

    improvements

--- app/src/main/java/com/example/groot/HomeActivity.kt
+++ app/src/main/java/com/example/groot/HomeActivity.kt
@@ -5,50 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-import com.example.groot.fragments.HelpFragment
-import com.example.groot.fragments.HomeFragment
-import com.example.groot.fragments.ProfileFragment
-import com.example.groot.fragments.SettingsFragment
-import com.example.groot.model.ViewPagerAdapter
+import com.example.groot.adapter.ViewPagerAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
-    private lateinit var navview: BottomNavigationView
-    private lateinit var viewpager: ViewPager2
+
+    private lateinit var navView: BottomNavigationView
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
-        navview=findViewById(R.id.bottom_navigation)
-        viewpager=findViewById(R.id.view_pager)
+        navView=findViewById(R.id.bottom_navigation)
+        viewPager = findViewById(R.id.viewPager)
+        window.statusBarColor = getColor(R.color.md_theme_surfaceContainer)
+        window.navigationBarColor = getColor(R.color.md_theme_surfaceContainer)
 
+        viewPager.adapter = ViewPagerAdapter(this)
 
-        viewpager.adapter= ViewPagerAdapter(this)
-        navview.setOnItemSelectedListener(){
+        navView.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.home->viewpager.currentItem=0
-                R.id.profile->viewpager.currentItem=1
-                R.id.help->viewpager.currentItem=2
-                R.id.settings->viewpager.currentItem=3
+                R.id.home -> viewPager.currentItem = 0
+                R.id.profile -> viewPager.currentItem = 1
+                R.id.help -> viewPager.currentItem = 2
+                R.id.settings -> viewPager.currentItem = 3
             }
             true
         }
 
-        viewpager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                when (position) {
-                    0 -> navview.selectedItemId = R.id.home
-                    1 -> navview.selectedItemId = R.id.profile
-                    2 -> navview.selectedItemId = R.id.help
-                    3 -> navview.selectedItemId = R.id.settings
+                when(position) {
+                    0 -> navView.selectedItemId = R.id.home
+                    1 -> navView.selectedItemId = R.id.profile
+                    2 -> navView.selectedItemId = R.id.help
+                    3 -> navView.selectedItemId = R.id.settings
                 }
             }
         })
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
